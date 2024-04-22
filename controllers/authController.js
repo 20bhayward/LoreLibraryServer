81acda7ef1887374acbde728f87a272accf91382
Novel <50412055+20bhayward@users.noreply.github.com>
2024-04-22 13:14:47 -0400

    fixed installs

--- controllers/authController.js
+++ controllers/authController.js
@@ -66,12 +66,6 @@ export const login = async (req, res) => {
 };
 
 export const logout = (req, res) => {
-  req.session.destroy((err) => {
-    if (err) {
-      console.error('Error destroying session:', err);
-      return res.status(500).json({ message: 'Internal server error' });
-    }
-    res.clearCookie('connect.sid'); // Clear the session cookie
-    res.json({ message: 'Logout successful' });
-  });
+  res.clearCookie('connect.sid'); // Clear the session cookie
+  res.json({ message: 'Logout successful' });
 }; 
